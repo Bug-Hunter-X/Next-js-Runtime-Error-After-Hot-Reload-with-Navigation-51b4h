0fba2e7a259273f0c64ee250d603b25902980479
BugHunter-X <askzenai@gmail.com>
2024-12-07 10:36:13 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,47 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+export default function About() {
+  // const router = useRouter();  // Removed useRouter
+  // const handleClick = () => {
+  //   router.push('/contact');
+  // };
+
+  return (
+    <div>
+      <h1>About Page</h1>
+      <Link href="/contact">
+        <a>Go to Contact Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/contact.js
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+    </div>
+  );
+}
+```
